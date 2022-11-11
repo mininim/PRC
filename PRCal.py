60015a47a6890bc08262ff832bcd22b2962eaff2
minsop <minsoplee2015@gmail.com>
2022-11-12 03:33:03 +0900

    Version 2 tableWidget으로 변경

--- PRCal.py
+++ PRCal.py
@@ -1,11 +1,10 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QCalendarWidget, QLabel, QGroupBox, QPushButton,QTextBrowser, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QCalendarWidget, QLabel, QGroupBox, QPushButton,QTextBrowser, QLineEdit, QTableWidget, QTableWidgetItem
 from PyQt5 import uic
 import sys
 
-
 class PRCal(QMainWindow):
 
-    events = {}
+    
 
     def __init__(self):
         super(PRCal, self).__init__()
@@ -13,16 +12,19 @@ class PRCal(QMainWindow):
         #Load the ui file
         uic.loadUi("PRCal.ui", self)
         
-        #Define widgets                       
+        #Define widgets  
+        self.events = {'2022-11-10': (['일일호프'], ['공운위', '학운위'])}                     
         self.calender = self.findChild(QCalendarWidget, "calendarWidget")
         self.newEventLine = self.findChild(QLineEdit, "lineEdit")
         self.addButton = self.findChild(QPushButton, "pushButton")
         self.stageEventTextBrowser = self.findChild(QTextBrowser, "textBrowser")
         self.dateLabel = self.findChild(QLabel, "data_label")
+        self.fixedList = self.findChild(QTableWidget,"tableWidget" )
+        self.waitingList = self.findChild(QTableWidget,"tableWidget_2" )
         
         #Connect the calender to the function   
         self.addButton.clicked.connect(self.addButton_click)
-        self.calender.clicked.connect(self.grab_date)
+        self.calender.clicked.connect(self.selectDate)
         
         
 
@@ -32,22 +34,70 @@ class PRCal(QMainWindow):
 
     def addButton_click(self):
         
-        self.events[str(self.calender.selectedDate().toPyDate())] = self.newEventLine.text()
-        self.stageEventTextBrowser.setText(self.newEventLine.text())
-
+        # 입력값 추가 
+        dateSelected = str(self.calender.selectedDate().toPyDate())
+        newEvent = self.newEventLine.text()
+        self.add(dateSelected, newEvent)
+        
+        #UI Update
+        #-linEdit update
         self.newEventLine.setText("")
+        #-Table update
+        self.updateLists(dateSelected)
+
         print(self.events)
 
-    def grab_date(self):
-        dateSelected = self.calender.selectedDate()
-        self.dateLabel.setText(str(dateSelected.toPyDate()))
-        
-        if str(dateSelected.toPyDate()) in self.events:
-            self.stageEventTextBrowser.setText(self.events[str(dateSelected.toPyDate())])
-        else:  
-            self.stageEventTextBrowser.setText(" ")
+
+
+
+
+
+    def selectDate(self):
+        # 선택된 날짜 
+        dateSelected = str(self.calender.selectedDate().toPyDate())
     
 
+        #UI Update
+        
+        #-label update
+        self.dateLabel.setText(dateSelected)
+        
+        #-Table update
+        self.updateLists(dateSelected)
+
+
+        #for testing  events
+        print(self.events)
+    
+
+    def add(self, date , newEvent):
+
+        if date in self.events:
+            self.events[date][0].append(newEvent)
+
+        else:
+            self.events[date] = ([],[])
+            self.events[date][0].append(newEvent)
+        
+
+    def updateLists(self, date):
+        if date in self.events:
+            row = 0 
+            
+            self.waitingList.setRowCount(len(self.events[date][0]))
+            self.fixedList.setRowCount(len(self.events[date][1]))
+            for e in self.events[date][0]:
+                self.waitingList.setItem(row, 0,  QTableWidgetItem(e ))
+                row += 1
+            
+            row = 0
+            for e in self.events[date][1]:
+                self.fixedList.setItem(row, 0,  QTableWidgetItem(e ))
+                row += 1
+
+        else: 
+            self.waitingList.setRowCount(0)
+            self.fixedList.setRowCount(0)
 
 
 if __name__ == "__main__":
